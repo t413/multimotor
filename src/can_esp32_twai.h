4b00d482cefb4dc6dd534bb78b383ba6eb021e8a
Tim O'Brien <timo@t413.com>
2025-07-22 17:17:43 -0700

    🦺 Adds ifdef checks around Arduino-specific TWAI header

--- src/can_esp32_twai.h
+++ src/can_esp32_twai.h
@@ -1,4 +1,5 @@
 #pragma once
+#if defined(ARDUINO) && defined(ESP32)
 #include "can_interface.h"
 #include <Arduino.h>
 
@@ -16,3 +17,5 @@ public:
     virtual CanMessage readOne();
     virtual String getAlerts();
 };
+
+#endif
