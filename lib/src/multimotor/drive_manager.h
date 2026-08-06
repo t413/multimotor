6f5d57444fad3a94c714067e4492a393d1460d34
Tim O'Brien <timo@t413.com>
2026-08-05 18:31:47 -0700

    DriveManager gets setup and actuate() methods

--- lib/src/multimotor/drive_manager.h
+++ lib/src/multimotor/drive_manager.h
@@ -5,6 +5,7 @@ class MotorDrive;
 class DriveManager {
 public:
     virtual void addDrive(MotorDrive* drive) = 0;
+    // virtual void setup() { }
     virtual MotorDrive* getDrive(uint8_t id) = 0;
     virtual MotorDrive* const* getDrives() const = 0;
     virtual uint8_t getCount() const = 0;
@@ -13,4 +14,5 @@ public:
     virtual bool handleIncoming(uint32_t id, uint8_t const* data, uint8_t len, uint32_t now) = 0; //parses incoming data, updates drives. returns true if handled
     virtual bool readOnce(uint32_t now, uint32_t timeout_us) = 0;
     virtual uint8_t iterate(uint32_t now, uint32_t timeout_ms) = 0; //reads port_ data if available, calls handleIncoming, returns new packet count
+    virtual void actuate(uint32_t now) { } //apply setpoints, if needed
 };
